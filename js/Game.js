6ac894a3a5f3cf0a7f24cede40fc93481f893589
JuniorLT <Lam2@calcharter.org>
2019-12-29 06:40:17 -0800

    Adding a start method
    Added a method to Game.js that will hide the startscreen and display the active random phrase

--- js/Game.js
+++ js/Game.js
@@ -10,14 +10,26 @@
      this.phrases = [{phrase: "Have Fun"},
      {phrase: "How are you"},
      {phrase: "Nice Job"},
-     {phrase: "The Force is Strong With This One"},
+     {phrase: "Thank You"},
      {phrase: "I am Your Father"}];
 
      this.activePhrase = 'null';
    }
+
    // gets a random phrase from the array above
    getRandomPhrase() {
     let randomNum = Math.floor(Math.random() * 5);
     return this.phrases[randomNum];
+   }
+
+   // Begins Game by selecting a random phrase and displaying
+   startGame() {
+     var startScreen = document.getElementById('overlay');
+     startScreen.style.display = "none";
+
+     var randomPhrase = this.getRandomPhrase();
+     var phrase = new Phrase(randomPhrase.phrase);
+     phrase.addPhraseToDisplay()
+     this.activePhrase = phrase;
    }
  }
