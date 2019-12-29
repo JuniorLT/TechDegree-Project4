243f9c5ba299fa6acc8e061bb6778e99aa14a3d3
JuniorLT <Lam2@calcharter.org>
2019-12-29 06:48:25 -0800

    Start game interaction
    Added an event listener to app.js that will wait unti the start game button is click and will initate the start game method on game.js

--- js/app.js
+++ js/app.js
@@ -2,3 +2,9 @@
  * Project 4 - OOP Game App
  * app.js */
 // Junior Lam Tiang
+
+var game = new Game();
+var button = document.getElementById('btn__reset');
+button.addEventListener('click', event => {
+  game.startGame();
+});
